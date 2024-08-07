10e5ece225430f093d5c21b2b6209c034039d33e
Vanshit Tyagi <contactvanshit@gmail.com>
2024-08-07 14:29:03 +0530

    Updates

---
+++ precache-manifest.5310c9c7b3c557392c981d19be848e28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f06975baf69836490dbedbc847e8cdb",
+    "url": "/Form-Submit-SmallPj/index.html"
+  },
+  {
+    "revision": "298be1b70d0446e03371",
+    "url": "/Form-Submit-SmallPj/static/css/main.12ad722d.chunk.css"
+  },
+  {
+    "revision": "c45eed0f1deafa8f0734",
+    "url": "/Form-Submit-SmallPj/static/js/2.c31c5e63.chunk.js"
+  },
+  {
+    "revision": "298be1b70d0446e03371",
+    "url": "/Form-Submit-SmallPj/static/js/main.8fdf7f60.chunk.js"
+  },
+  {
+    "revision": "290a07583ddb330b637b",
+    "url": "/Form-Submit-SmallPj/static/js/runtime~main.fe809783.js"
+  }
+]);
